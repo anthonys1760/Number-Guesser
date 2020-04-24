b68bbbf223ba49956df7f101a3a2afdf9a33073a
Anthony <anthony.smith1760@gmail.com>
2020-04-24 14:19:43 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/*
+GAME FUNCTION:
+-Player must guess a number netween a min and max
+-Player gets a certain amount of guesses
+-Notify player of guesses remainin
+-Notify the player of the correct answer if loose
+-Let player choose to play again
+*/
+
