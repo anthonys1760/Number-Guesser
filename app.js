1be4a950803949409a411d585fd56160da448577
Anthony <anthony.smith1760@gmail.com>
2020-04-24 15:58:46 -0400

    Completed Project

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ GAME FUNCTION:
 //Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 //UI Elements
-const game = document.querySelector('game');
+const game = document.querySelector('#game');
 const minNum = document.querySelector('.min-num');
 const maxNum = document.querySelector('.max-num');
 const guessBtn = document.querySelector('#guess-btn');
@@ -25,10 +25,17 @@ const message = document.querySelector('.message');
 minNum.textContent = min;
 maxNum.textContent = max;
 
+//Play again event listener
+game.addEventListener('mousedown', function(e) {
+    if (e.target.className === 'play-again') {
+        window.location.reload();
+    }
+    
+});
 //Listen for guess
 guessBtn.addEventListener('click', function () {
     let guess = parseInt(guessInput.value);
-    
+
     //Validate 
     if (isNaN(guess) || guess < min || guess > max) {
         setMessage(`Please enter a number between ${min} and ${max}`, 'red');
@@ -36,16 +43,53 @@ guessBtn.addEventListener('click', function () {
 
     //Check if won
     if (guess === winningNum) {
-        //Disable input
-        guessInput.disabled = true;
-        //Change border color
-        guessInput.style.borderColor = 'green';
-        //Set message
-        setMessage(`${winningNum} is correct! YOU WIN!`, 'green');
-    } else {
+        //Game over - Won
 
+        gameOver(true, `${winningNum} is correct! YOU WIN!`);
+
+    } else {
+        //Wrong number
+        guessesLeft -= 1;
+
+        if (guessesLeft === 0) {
+            //Game over - lost
+            gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`, 'red');
+        } else {
+            //Game continues - answer wrong
+            setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+
+            //Clear input
+            guessInput.value = '';
+
+            //Tell user its the wrong number
+            guessInput.style.borderColor = 'red';
+        }
     }
 });
+
+//Game Over
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+
+    //Disable input
+    guessInput.disabled = true;
+    //Change border color
+    guessInput.style.borderColor = 'green';
+    //Set text color
+    message.style.color = color;
+    //Set message
+    setMessage(msg);
+
+    // Play Again?
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+}
+
+//Get Winning Number
+function getRandomNum(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
 
 //Set message 
 function setMessage(msg, color) {
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Number Guesser
+ 
+The computer will randomly select a number between 1 and 10 and you have 3 tries to guess what the number is. 
+
+![title-pic](guesser.png)
+ 
+## Steps to execute this webpage:
+- Download the entire code 
+- Open up the index.html.
+- [View Live Site](https://anthonys1760.github.io/Simple-Calculator/)
+
+## Technologies used: 
+- HTML
+- CSS 
+- Javascript
+
